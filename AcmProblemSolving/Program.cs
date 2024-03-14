e63e8c2cfd020f213b0320c6784a4905fe9d8320
Washiq Shamsi <jakas1gy@gmail.com>
2024-03-14 23:21:55 +0600

    Euler 29

--- AcmProblemSolving/Program.cs
+++ AcmProblemSolving/Program.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 
 namespace AcmProblemSolving
@@ -13,17 +14,11 @@ namespace AcmProblemSolving
     {
         public static void Main(string[] args)
         {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-
-            //Do Code
-            Console.WriteLine(new Euler15().WayCount(20,20, 0, 0));
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine("Time: "+elapsedMs +" ms");
-
+           
         }
 
     }
 }
+
 
 
---
+++ ProjectEular/Euler29.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ProjectEular
+{
+    public class Euler29
+    {
+        public void Solution()
+        {
+            BigInteger l = 2, h = 100;
+            int exl = 2, exh = 100;
+
+            List<BigInteger> values = new List<BigInteger>();
+
+            for (BigInteger i = l; i <= h; i++)
+            {
+                for (int j = exl; j <= exh; j++)
+                {
+                    BigInteger val = BigInteger.Pow(i, j);
+                    values.Add(val);
+                }
+            }
+
+            int ans = values.Distinct().Count();
+            Console.WriteLine(ans);
+        }
+    }
+}
